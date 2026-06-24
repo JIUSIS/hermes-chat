f9dcf5ef82f439547721fcd198cf578f87b3220e
九思 <eiol@qq.com>
2026-06-24 16:26:36 +0800

    fix: hardcode compileSdk 36 for file_picker plugin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.hermes.hermes_chat"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
     buildToolsVersion = "36.0.0"
 
